c6268e29309482dbc2db9c9f04c8dcad6e1d9640
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-13 23:14:03 +0300

    feat: add PageLayout component

---
+++ src/shared/ui/page-layout/index.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+interface PageLayoutProps {
+  content?: ReactNode;
+}
+
+export const PageLayout = ({ content }: PageLayoutProps) => {
+  return <>{content}</>;
+};
