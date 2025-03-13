ecbb9aabf243fb3a7cd0132bbbae92c631929bf8
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-13 22:59:11 +0300

    feat: add userModel slice

---
+++ src/entities/user/model/user.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { UserRole } from 'shared/types/common.types';
+import { User } from 'entities/user/types';
+import { TCustomSelector } from 'shared/types/store.types';
+
+export type UserState = {
+  _id: string;
+  role: UserRole | null;
+  data: User | null;
+  isLoading: boolean;
+  isFailed: boolean;
+  error?: string | null | undefined;
+};
+
+const initialState: UserState = {
+  _id: '',
+  role: null,
+  data: null,
+  isLoading: false,
+  isFailed: false,
+  error: null,
+};
+
+export const isRootSelector: TCustomSelector<boolean> = state =>
+  !!state.user && (state.user.data?.isRoot ?? false);
+
+export const userModel = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, { payload }: PayloadAction<User>) => {
+      state.data = payload;
+    },
+  },
+});
