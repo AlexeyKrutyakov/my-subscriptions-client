74ef0f26f586069b42a13a435436efee77518bfa
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-13 22:44:38 +0300

    feat: add RootState and AppDispatch

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -4,3 +4,6 @@ export const store = configureStore({
   reducer: {},
   middleware: getDefaultMiddleware => getDefaultMiddleware(),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
