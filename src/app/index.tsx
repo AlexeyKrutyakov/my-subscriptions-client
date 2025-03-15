bfff015421ed9a6b61900e400db206974f0a7d4a
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-15 20:47:46 +0300

    fix: return AppRoutes in App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,7 @@
 import { AppRoutes } from './routing/app-routes';
-import { PageLayout } from '../shared/ui/page-layout/page-layout';
 
 function App() {
-  return <PageLayout content={<AppRoutes />} />;
+  return <AppRoutes />;
 }
 
 export default App;
