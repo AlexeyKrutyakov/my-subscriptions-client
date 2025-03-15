0fd3f283c009ecebe8d40b15e5df0e28b7ac3e48
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-15 20:53:11 +0300

    feat: add reducer to store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { userModel } from '../../entities/user/model/user';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userModel.reducer,
+  },
   middleware: getDefaultMiddleware => getDefaultMiddleware(),
 });
 
