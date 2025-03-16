bc70ccf5c7e56a0633c4ab594f9b2a8126959aa9
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-16 20:39:46 +0300

    refactor: add functionality for TempStateManager

--- src/shared/ui/temp-state-manager/temp-state-manager.tsx
+++ src/shared/ui/temp-state-manager/temp-state-manager.tsx
@@ -1,12 +1,30 @@
-import Button from '../button/button';
+import { useAppDispatch } from '../../../app/hooks';
 import styles from './temp-state-manager.module.css';
+import { setUser, setUserRole } from '../../../entities/user/model/user';
+import { User } from '../../../entities/user/types';
+import { userRole } from '../../../shared/types/common.types';
+
+const user: User | null = {
+  _id: '123',
+  name: 'Alex',
+  avatar: 'avatar',
+  role: userRole.ADMIN,
+  // role: userRole.CLIENT,
+};
 
 const TempStateManager = () => {
+  const dispatch = useAppDispatch();
+
+  const setUserHandler = () => {
+    dispatch(setUser(user));
+    dispatch(setUserRole(user.role));
+  };
+
   return (
     <div className={styles.manager}>
       <h1>State manager (temp)</h1>
       <section className={styles.btns}>
-        <Button value="Set role" />
+        <button onClick={setUserHandler}>Set user and role</button>
       </section>
     </div>
   );
