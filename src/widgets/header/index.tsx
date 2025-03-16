1060e3939e47283e4683fd756e11d0fd1b555775
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-16 17:13:59 +0300

    feat: add temp styles to Header

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,5 +1,7 @@
+import styles from './styles.module.css';
+
 const Header = () => {
-  return <div>Header</div>;
+  return <section className={styles.header}>Header</section>;
 };
 
 export default Header;
