46efa800367f08e3496ea747a5721299ce0e2d28
AlexeyKrutyakov <coder.arctik@gmail.com>
2025-03-16 17:08:48 +0300

    feat: add buttons to UnauthPage

--- src/pages/unauth/index.tsx
+++ src/pages/unauth/index.tsx
@@ -1,3 +1,13 @@
+import Button from '../../shared/ui/button/button';
+import styles from './styles.module.css';
+
 export function UnauthPage() {
-  return <>Зарегистрируйтесь</>;
+  return (
+    <>
+      <section className={styles.btns}>
+        <Button value="Login" />
+        <Button value="Registration" />
+      </section>
+    </>
+  );
 }
